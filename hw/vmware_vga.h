a583105779e6765a36cfad1bf57f225059ebf01d
Don Slutz <Don@CloudSwitch.com>
2012-10-18 10:14:57 -0400

    Fix slot id for vmware-svga
    Signed-off-by: Don Slutz <Don@CloudSwitch.com>

--- hw/vmware_vga.h
+++ hw/vmware_vga.h
@@ -3,12 +3,17 @@
 
 #include "qemu-common.h"
 
+#ifndef VMARE_MODE_DEF
+#define VMARE_MODE_DEF
+extern int vmware_mode;
+#endif
+
 /* vmware_vga.c */
 static inline DeviceState *pci_vmsvga_init(PCIBus *bus)
 {
     PCIDevice *dev;
-
-    dev = pci_create_simple(bus, -1, "vmware-svga");
+    
+    dev = pci_create_simple(bus, vmware_mode ? PCI_DEVFN(0xf, 0) : -1, "vmware-svga");
     return &dev->qdev;
 }
 
