a72354aa7240ac358cc70e7aa33a603abb651571
Don Koch <dkoch@cloudswitch.com>
2013-10-17 19:07:45 +0000

    HV-185: Don't crash on no input handler.
    We now send a message to the log and punt the call to the handler.

--- ui/input.c
+++ ui/input.c
@@ -414,7 +414,10 @@ void kbd_put_keycode(int keycode)
         return;
     }
     if (entry) {
-        entry->put_kbd(entry->opaque, keycode);
+	if (entry->put_kbd)
+	    entry->put_kbd(entry->opaque, keycode);
+	else
+	    printf("%s: no keyboard handler\n", __FUNCTION__);
     }
 }
 
