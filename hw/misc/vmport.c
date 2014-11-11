35935b8255b80612b3c1837c08b4ce3f4e4fb3a7
Don Slutz <dslutz@verizon.com>
2014-11-11 13:28:46 -0500

    vmport: Fix vmport_cmd_ram_size
    Based on
    
    https://sites.google.com/site/chitchatvmback/backdoor
    
    and testing on ESXi, this should be in MB not bytes.
    
    Signed-off-by: Don Slutz <dslutz@verizon.com>
    CC: Don Slutz <don.slutz@gmail.com>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/misc/vmport.c
+++ hw/misc/vmport.c
@@ -112,7 +112,7 @@ static uint32_t vmport_cmd_ram_size(void *opaque, uint32_t addr)
     X86CPU *cpu = X86_CPU(current_cpu);
 
     cpu->env.regs[R_EBX] = 0x1177;
-    return ram_size;
+    return ram_size >> 20; /* in MB */
 }
 
 /* vmmouse helpers */
